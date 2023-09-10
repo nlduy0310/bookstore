ea7ad72c2b43a217c0669a2881b480b2db9e53ec
nlduy0310 <nlduy0310@gmail.com>
2023-09-11 00:39:37 +0700

    add basic functionalities: create books, show books, edit books, delete books

--- backend/routes/booksRoute.js
+++ backend/routes/booksRoute.js
@@ -88,7 +88,6 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
 	try {
 		const { id } = req.params;
-
 		const result = await Book.findByIdAndDelete(id);
 
 		if (!result) {
--- frontend/pages/CreateBook.jsx
+++ frontend/pages/CreateBook.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const CreateBook = () => {
-  return (
-    <div>CreateBook</div>
-  )
-}
+	const [title, setTitle] = useState('');
+	const [author, setAuthor] = useState('');
+	const [publishYear, setPublishYear] = useState('');
+	const [loading, setLoading] = useState(false);
+	const navigate = useNavigate();
 
-export default CreateBook
+	const handleCreation = () => {
+		const bookData = {
+			title,
+			author,
+			publishYear,
+		};
+
+		axios
+			.post('http://localhost:2000/books', bookData)
+			.then(() => {
+				setLoading(false);
+				navigate('/');
+			})
+			.catch((error) => {
+				alert('An error happened. Please try again!');
+				console.error(error);
+				setLoading(false);
+			});
+	};
+
+	return (
+		<div className="p-4">
+			<BackButton />
+			<h1 className="text-3xl my-4">Create Book</h1>
+			{loading ? <Spinner /> : ''}
+			<div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
+				<div className="my-4">
+					<label className="text-xl mr-4 text-gray-500">Title</label>
+					<input
+						type="text"
+						value={title}
+						onChange={(event) => setTitle(event.target.value)}
+						className="border-2 border-gray-500 px-4 py-2 w-full"
+					/>
+					<label className="text-xl mr-4 text-gray-500">Author</label>
+					<input
+						type="text"
+						value={author}
+						onChange={(event) => setAuthor(event.target.value)}
+						className="border-2 border-gray-500 px-4 py-2 w-full"
+					/>
+					<label className="text-xl mr-4 text-gray-500">Publish Year</label>
+					<input
+						type="text"
+						value={publishYear}
+						onChange={(event) => setPublishYear(event.target.value)}
+						className="border-2 border-gray-500 px-4 py-2 w-full"
+          />
+          <button className='p-2 bg-sky-300 m-8' onClick={handleCreation}>
+            Save
+          </button>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default CreateBook;
